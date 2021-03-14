76562fcb8777bfbec7f8ab8b10443f61f016dfdc
BonusGned <jesus.moj@mail.ru>
2021-03-14 14:03:18 +0300

    routes saved

--- routes/forms.py
+++ routes/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 
 from cities.models import City
+from routes.models import Route
+from trains.models import Train
 
 
 class RouteForm(forms.Form):
@@ -26,3 +28,28 @@ class RouteForm(forms.Form):
             'placeholder': 'Travel time'
         })
     )
+
+
+class RouteModelForm(forms.ModelForm):
+    name = forms.CharField(
+        label='Название маршрута', widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Введите название маршрута'
+        }))
+    from_city = forms.ModelChoiceField(
+        queryset=City.objects.all(), widget=forms.HiddenInput()
+    )
+    to_city = forms.ModelChoiceField(
+        queryset=City.objects.all(), widget=forms.HiddenInput()
+    )
+    trains = forms.ModelMultipleChoiceField(
+        queryset=Train.objects.all(),
+        required=False, widget=forms.SelectMultiple(
+            attrs={'class': 'form-control d-none'}
+        )
+    )
+    travel_times = forms.IntegerField(widget=forms.HiddenInput())
+
+    class Meta:
+        model = Route
+        fields = '__all__'
